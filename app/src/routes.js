563327ab5c5a2a8490e6cc40dcc04e60e62467fe
Mugsy Carter <mugsycarter@gmail.com>
2017-08-14 17:17:46 -0700

    added main.js, index.html, and routes.js to the src directory in app

---
+++ app/src/routes.js
@@ -0,0 +1,41 @@
+routes.$inject = ['$stateProvider', '$urlRouterProvider']; 
+
+export default function routes($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider.state({
+        name: 'home',
+        url: '/',
+        data: { public: true },
+        component: 'home' 
+    });
+
+    // $stateProvider.state({
+    //     name: 'play',
+    //     url: '/play',
+    //     data: { public: true },
+    //     component: 'play' 
+    // });
+
+    // $stateProvider.state({
+    //     name: 'rules',
+    //     url: '/rules',
+    //     data: { public: true },
+    //     component: 'rules' 
+    // });
+
+    // $stateProvider.state({
+    //     name: 'settings',
+    //     url: '/settings',
+    //     data: { public: true },
+    //     component: 'settings' 
+    // });
+
+    // $stateProvider.state({
+    //     name: 'about',
+    //     url: '/about',
+    //     data: { public: true },
+    //     component: 'about' 
+    // });
+
+    $urlRouterProvider.otherwise('/');
+}
